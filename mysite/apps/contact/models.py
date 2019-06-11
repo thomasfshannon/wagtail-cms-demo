05f789352e2ea20e2a1ff0188d263034b16fa37d
Thomas Shannon <thomasshannon1117@yahoo.com>
2019-06-10 18:56:46 -0700

    - added quick submission success and clear form - fixed model reference to use EmailField rather than charfield

--- mysite/apps/contact/models.py
+++ mysite/apps/contact/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class ContactSubmission(models.Model):
-    email = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
     message = models.TextField()
 
     def __str__(self):
